598e255199fe208f8ca66b3a331652414a28666f
Udesh <udesh@wisesoftlab.com>
2024-04-22 22:32:39 +0530

    configured env

--- src/app/core/config/database/database.constants.ts
+++ src/app/core/config/database/database.constants.ts
@@ -1,3 +1,2 @@
 // database.constants.ts
-export const CONNECTION_STRING =
-  'mongodb+srv://wudeshp:Hzr6TS8PtQBtSBJC@propertyrentalplatformc.l46okbo.mongodb.net/property_rental_platform_db?retryWrites=true&w=majority&appName=PropertyRentalPlatformCluster';
+export const CONNECTION_STRING = 'mongodb+srv://wudeshp:Hzr6TS8PtQBtSBJC@propertyrentalplatformc.l46okbo.mongodb.net/property_rental_platform_db?retryWrites=true&w=majority&appName=PropertyRentalPlatformCluster';
